e79132196009c50f75096c9571fd50f687b52731
Tyler-the-Wizard <69734259+Tyler-the-Wizard@users.noreply.github.com>
2022-12-15 15:56:16 -0700

    view.py - view the heightmap with color coding

---
+++ 2022/day-12/view.py
@@ -0,0 +1,12 @@
+with open('input.txt') as file:
+    data = file.readlines()
+
+def colorize_char(ch):
+    i = (ord(ch) - ord('a')) % 8
+    return f'\33[4{i}m{ch}\33[0m'
+
+def colorize(s):
+    return ''.join(colorize_char(c) for c in s)
+
+for line in data:
+    print(colorize(line), end='')
